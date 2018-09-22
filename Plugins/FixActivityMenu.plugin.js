9c294b08b0d5f5fdb8d58ed11484fff394e2af18
CompletelyUnbelievable <39345185+CompletelyUnbelievable@users.noreply.github.com>
2018-09-21 23:28:32 -0700

    Update FixActivityMenu.plugin.js

--- Plugins/FixActivityMenu.plugin.js
+++ Plugins/FixActivityMenu.plugin.js
@@ -1,6 +1,7 @@
 //META{"name":"FixActivityMenu"}*//
 
-var PluginName, PluginDesc, PluginVers, PluginAuth;
+var PluginName, PluginDesc, PluginVers, PluginAuth, observer;
+
 
 class FixActivityMenu {
 
@@ -21,6 +22,7 @@ class FixActivityMenu {
 		removeClass('background-1ri_HN', 'CUReplaced');
 		removeClass('background-3zYSQO', 'CUReplaced');
 		removeClass('splashArt-1anaP9', 'CUReplaced');
+
 		if (document.getElementById(PluginName + 'CSS')) {
 			BdApi.clearCSS(PluginName + 'CSS');
 		}
@@ -33,30 +35,30 @@ class FixActivityMenu {
 
 function setStyles() {
 	var styles = `#app-mount .news-2GDtLJ {
-	background: transparent;
-}
-    
-#app-mount .body-3NySJS, .article-3kb3qm > div:not(.background-1ri_HN) {
-	background: rgba(46,49,54,0.9);
- 	padding: 10px 10px 10px 10px;
- 	border-radius: 10px;
- 	margin: 0 auto 0 auto;
-}
-    
-#app-mount .article-3kb3qm, #app-mount .carousel-JbsNzL, #app-mount .paginationItem-3-MBTL {
-	background: transparent;
-}
-    
-#app-mount .background-1ri_HN, #app-mount .background-3zYSQO {
-	height: 100%;
-	background-size: cover;
-}
-    
-#app-mount .background-1ri_HN, #app-mount .storePromotionBackground-3DvkVd, #app-mount .splashArt-1anaP9, #app-mount .background-3zYSQO {
-	-webkit-mask: none;
-	filter: none;
-	opacity: 0.9;
-}`;
+		background: transparent;
+	}
+	
+	#app-mount .body-3NySJS, .article-3kb3qm > div:not(.background-1ri_HN) {
+		background: rgba(46,49,54,0.9);
+		padding: 10px 10px 10px 10px;
+		border-radius: 10px;
+		margin: 0 auto 0 auto;
+	}
+	
+	#app-mount .article-3kb3qm, #app-mount .carousel-JbsNzL, #app-mount .paginationItem-3-MBTL {
+		background: transparent;
+	}
+	
+	#app-mount .background-1ri_HN, #app-mount .background-3zYSQO {
+		height: 100%;
+		background-size: cover;
+	}
+	
+	#app-mount .background-1ri_HN, #app-mount .storePromotionBackground-3DvkVd, #app-mount .splashArt-1anaP9, #app-mount .background-3zYSQO {
+		-webkit-mask: none;
+		filter: none;
+		opacity: 0.9;
+	}`;
 
 	return styles;
 }
@@ -75,7 +77,7 @@ function FindModify(x, y) { /*Find x, remember with y.*/
 function removeClass(x, y) { /*Find x, remove with y.*/
 	if (document.getElementsByClassName(x, y)) {
 		$('.' + x).each(function(i, obj) {
-			document.getElementsByClassName(x)[i].classList.remove(y);
+		document.getElementsByClassName(x)[i].classList.remove(y);
 		});
 	}
 }
@@ -84,11 +86,11 @@ function AmendString(x) {
 
 	if (x.lastIndexOf(".") > -1 && x.lastIndexOf(".") + 15 != x.length) {
 		x = x.slice(0, x.lastIndexOf(".") + 4);
-		x += "?format=png"; /*Prevents Gifs from playing, this would easily be a setting.*/
-	}
+		x += "?format=png"; /*Prevents Gifs from playing, this could easily be a setting.*/
+	 }
 
 	if (x.lastIndexOf(".") + 15 == x.length) {
-		console.log("Repeat"); /*Should not be seen if everything is going well.*/
+		console.log("Repeat") /*Should not be seen if everything is going well.*/
 	}
 
 	return (x);
