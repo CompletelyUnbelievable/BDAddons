c08c8030eea37676269a297cc9fe27786c18856c
CompletelyUnbelievable <39345185+CompletelyUnbelievable@users.noreply.github.com>
2019-07-17 03:18:32 -0700

    Update VCButton.plugin.js

--- PluginsExperiments/VCButton.plugin.js
+++ PluginsExperiments/VCButton.plugin.js
@@ -18,7 +18,7 @@ class VCButton{
 		this.changeChannel=BdApi.findModuleByProps("selectChannel","selectVoiceChannel").selectChannel;//function(guildId,channelId,?messageId) *strings*
 		this.type=BdApi.findModuleByProps("ChannelTypes").ChannelTypes||{GUILD_TEXT:0,DM:1,GUILD_VOICE:2,GROUP_DM:3,GUILD_CATEGORY:4,GUILD_NEWS:5,GUILD_STORE:6,GUILD_LFG_LISTINGS:7};
 		this.classes={channels:window.ZLibrary?window.ZLibrary.DiscordClasses.ChannelList.channels.first:"channels-Ie2l6A",channelName:"name-3_Dsmg",channelContainer:window.ZLibrary?window.ZLibrary.DiscordClasses.ChannelList.containerDefault.first:"containerDefault-1ZnADq",channelChildren:"children-Bmpf2Q"};
-		this.ReactTools=window.ZLibrary&&window.ZLibrary.ReactTools&&window.ZLibrary.ReactTools.getOwnerInstance&&this.testFunction(window.ZLibrary.ReactTools.getOwnerInstance,window.ZLibrary.ReactTools,document.body)?window.ZLibrary.ReactTools.getOwnerInstance.bind(window.ZLibrary.ReactTools):this.findReactComponent;
+		this.ReactTools=window.ZLibrary&&window.ZLibrary.ReactTools&&window.ZLibrary.ReactTools.getOwnerInstance&&this.testFunction(window.ZLibrary.ReactTools.getOwnerInstance,window.ZLibrary.ReactTools,document.body)!==false?window.ZLibrary.ReactTools.getOwnerInstance.bind(window.ZLibrary.ReactTools):this.findReactComponent;
 		this.currentVCId=BdApi.findModuleByProps("getChannelId","getVoiceChannelId").getVoiceChannelId;
 		this.clipboard=window.require?require('electron').remote.clipboard:'';
 		this.cleanup=[];//Array of functions, these functions will remove elements from the document that were added by the plugin.
